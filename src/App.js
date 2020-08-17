93c8cd7547ce7e2359a3eff4a07712864a2353bf
MMMarz009 <mar0099887@gmail.com>
2020-08-17 16:12:58 -0500

    Delected unnecessary files

--- src/App.js
+++ src/App.js
@@ -5,20 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* The landing page of my portfolio goes here
+      it will have Resume, About Me, Projects, & Contact */}
     </div>
   );
 }
